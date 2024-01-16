de0bbf1ba0e9387805f507f5d867d50ceacad144
pavel <pavel@setupad.com>
2024-01-16 18:38:10 +0200

    object introduction

--- arrays/forCiklai.js
+++ arrays/forCiklai.js
@@ -1,12 +1,20 @@
 //               0      1       2       3        4          5
 let animals = ['cat', 'dog', 'mouse', 'horse', 'bird', 'elephant'];
 
-for (let i = 0; i < animals.length; i++) {
-  console.log('indeksas: ', i, 'reiksme: ', animals[i]);
-}
+// for (let i = 0; i < animals.length; i++) {
+//   console.log('indeksas: ', i, 'reiksme: ', animals[i]);
+// }
 
 // -----------------------------------------------
 
-for (let value of animals) {
-  console.log(value);
+// for (let value of animals) {
+//   console.log(value);
+// }
+
+let vidurkis = 0;
+let suma = 0;
+
+if (typeof vidurkis === 'number') {
+  console.log('vidurkis: ', typeof vidurkis);
+  console.log('suma: ', typeof suma);
 }
---
+++ objects/script.js
@@ -0,0 +1,42 @@
+let student = {
+  vardas: 'Pavelas',
+  kursas: 'JavaScript',
+  metai: 2023,
+  sodra: true,
+  pazymiai: [10, 2, 8, 4],
+  masina: {
+    marke: 'Audi',
+    modelis: 'A6',
+    metai: 2018,
+    spalva: 'raudona',
+  },
+};
+
+// taip galime pakeisti rakto reiksme objekte
+student.kursas = 'Node.js';
+
+console.log(student);
+
+console.log(student.pazymiai[3]);
+console.log(student.masina.spalva);
+
+// apacioje yra masyvas kurio reiksmes yra objektai, panasiai kaip const arr = ["dog", "cat"]
+// tik vietoj stringu "dog" ir "cat" yra objektai
+const students = [
+  {
+    vardas: 'Pavelas',
+    kursas: 'JavaScript',
+    metai: 2023,
+    sodra: true,
+    pazymiai: [10, 2, 8, 4],
+  },
+  {
+    vardas: 'Evaldas',
+    kursas: 'Node.js',
+    metai: 2023,
+    sodra: true,
+    pazymiai: [10, 10, 9, 4],
+  },
+];
+
+console.log(students[0]);
