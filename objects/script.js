c70eae91d25cfe2126f00fd6c26da59339b57303
pavel <pavel@setupad.com>
2024-01-16 21:42:12 +0200

    object loops

--- objects/script.js
+++ objects/script.js
@@ -15,10 +15,10 @@ let student = {
 // taip galime pakeisti rakto reiksme objekte
 student.kursas = 'Node.js';
 
-console.log(student);
+// console.log(student);
 
-console.log(student.pazymiai[3]);
-console.log(student.masina.spalva);
+// console.log(student.pazymiai[3]);
+// console.log(student.masina.spalva);
 
 // apacioje yra masyvas kurio reiksmes yra objektai, panasiai kaip const arr = ["dog", "cat"]
 // tik vietoj stringu "dog" ir "cat" yra objektai
@@ -39,4 +39,23 @@ const students = [
   },
 ];
 
-console.log(students[0]);
+// console.log(students[0]);
+
+// parduotuve
+
+console.log('----parduotuve----');
+
+const parduotuve = {
+  pavadinimas: 'Maxima',
+  adresas: 'Savanoriu pr. 1',
+  darboLaikas: {
+    nuo: 10,
+    iki: 22,
+  },
+};
+
+// turime du variantus kaip paimti objekto reiksme
+// pirmas variantas:
+console.log(parduotuve.pavadinimas);
+// antras variantas:
+console.log(parduotuve['pavadinimas']);
