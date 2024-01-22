b6405a2b86f5a6e007cd962c248703f8ae458bc5
pavel <pavel@setupad.com>
2024-01-22 20:00:56 +0200

    arrow function

---
+++ functions/arrowFunk.js
@@ -0,0 +1,9 @@
+let defaultFunk = function () {
+  return 'Hello, this is default function';
+};
+
+let arrowFunk = () => {
+  return 'Hello, this is arrow function';
+};
+
+console.log(arrowFunk());
