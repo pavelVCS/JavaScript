b4013f4a5ffba9b568da48b6943b01e4fd2c5ca0
pavel <pavel@setupad.com>
2024-08-07 18:06:05 +0300

    update operators script.js

---
+++ operators/script.js
@@ -0,0 +1,20 @@
+let dalyba = 3 / 2; // 3 / 2 = 1.5
+let modulis = 4 % 3; // 4 / 3 = 1 ir liekana 1
+
+let x = 1;
+let y = 2;
+let result = x + y; // 3
+let z = 4;
+//         3      4
+// result = result + z; // 7
+result += z; // 7
+
+let a = 5;
+
+// a = a + 1; // 6
+// a += 1; // 6
+// a++; // 6
+//           a lygu:    5
+console.log('a lygu:', a++);
+//           a du lygu:   6
+console.log('a du lygu:', a);
