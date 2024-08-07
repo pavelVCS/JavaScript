2d0c08f87da75066675f34206a95b4c9ee3378c7
pavel <pavel@setupad.com>
2024-08-07 21:19:09 +0300

    add logic operators

--- ifStatement/elseif.js
+++ ifStatement/elseif.js
@@ -7,3 +7,29 @@ if (age >= 18) {
 } else {
   console.log('Jūs esate vaikas');
 }
+
+// logic operators
+if (age < 18) {
+  if (age >= 16) {
+    // kazkoks kodas
+  }
+}
+
+//   true    &&    false
+if (age < 18 && age >= 16) {
+  // kazkoks kodas
+}
+
+// DRY - Don't Repeat Yourself
+// WET - Write Everything Twice
+let car = 'Audi';
+let carYear = 2009;
+
+if (car === 'BMW' || car === 'Audi') {
+  // grazinti automobilio duomenis
+}
+
+if (carYear < 2010 && (car === 'BMW' || car === 'Audi')) {
+  // grazinti automobilio duomenis
+  console.log('Masina:', car, 'Metai:', carYear);
+}
