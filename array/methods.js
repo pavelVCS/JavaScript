8d4f068788bf0bba5390172d5989a31ea069dc75
pavel <pavel@setupad.com>
2024-08-28 20:33:14 +0300

    introduce array method reduce

--- array/methods.js
+++ array/methods.js
@@ -4,5 +4,22 @@ let duplicate = [0, ...arr, 7, 8]; // copy all elements of arr into duplicate
 
 arr.push(4);
 
-console.log('duplicate', duplicate);
-console.log('arr', arr);
+// console.log('duplicate', duplicate);
+// console.log('arr', arr);
+
+// iterate over array
+
+let names = ['John', 'Jane', 'Doe'];
+
+names.forEach((name, ind) => {
+  //   console.log(name, 'invited to the party');
+  //   console.log('index:', ind);
+});
+
+// reduce
+
+const numbers = [1, 2, 3, 4, 5];
+
+let result = numbers.reduce((res, number) => res + number, 0);
+
+console.log('result', result);
