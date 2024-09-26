4ee6573e21cb858c72a086e1085eaee42fb7cec3
pavel <pavel@setupad.com>
2024-09-26 19:03:47 +0300

    async await function

--- async/user.js
+++ async/user.js
@@ -1,13 +1,38 @@
-fetch('https://jsonplaceholder.typicode.com/users/1')
-  .then((response) => {
-    return response.json();
-  })
-  .then((user) => {
-    console.log(user);
+// fetch('https://jsonplaceholder.typicode.com/users/1')
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((user) => {
+//     console.log(user);
 
-    printUser(user);
-  })
-  .catch((error) => {
-    console.log(error);
-    document.body.innerHTML = 'An error has occurred. Please try again later.';
-  });
+//     printUser(user);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//     document.body.innerHTML = 'An error has occurred. Please try again later.';
+//   });
+
+// async function
+async function fetchUser() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users/2');
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch user');
+    }
+
+    const user = await response.json();
+
+    console.log(user);
+  } catch (error) {
+    console.log(error.message);
+    document.body.innerHTML = error.message
+      ? error.message
+      : 'An error has occurred. Please try again later.';
+  }
+}
+
+// async arrow function
+const fetchUserArrow = async () => {};
+
+fetchUser();
